fd6575c959429bcc9e076954c21656c23fea6c74
Podze Aleksandr <dasdeg@gmail.com>
2014-10-19 22:53:43 +0300

    Routine to read from file; added Row and enums

--- kddNeural/KddNetwork.cs
+++ kddNeural/KddNetwork.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Deployment.Application;
+using System.IO;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using AForge.Neuro;
@@ -12,6 +14,7 @@ namespace kddNeural
         public long LineCount { get; set; }
         public bool Active { get; set; }
         public string FilePath { get; set; }
+        private Thread T { get; set; }
         public KddNetwork(string filePath, long fromLine, long count)
         {
             FromLine = fromLine;
@@ -22,13 +25,16 @@ namespace kddNeural
         public void StartLearning()
         {
             Active = true;
-            var t = new Thread(learnThreadProcedure);
-            throw new NotImplementedException();
+            T = new Thread(learnThreadProcedure);
+            T.Start();
+            //throw new NotImplementedException();
         }
 
         public void Cancel()
         {
-            throw new NotImplementedException();
+            Active = false;
+            if (T.IsAlive)
+                T.Abort();
         }
 
         public void TestInput(long testLine, string filePath)
@@ -36,9 +42,64 @@ namespace kddNeural
             throw new NotImplementedException();
         }
 
+        private struct Row
+        {
+            public Row(string[] row)
+            {
+                Duration = int.Parse(row[0]);
+                Output = RowHelper.GetEnumFromString<Protocol>(row[1]);
+                Input = RowHelper.GetEnumFromString<Protocol>(row[2]);
+
+                GenConType = RowHelper.GetGenericConnectionType(row[row.Length]);
+                MidConType = RowHelper.GetMiddleSpecificConnectionType(row[row.Length]);
+                ConType = RowHelper.GetEnumFromString<SpecificConnectionType>(row[row.Length]);
+
+                intParams = new int[22];
+                for (int i = 0; i < 20; i++)
+                {
+                    intParams[i] = int.Parse(row[i + 4]);
+                }
+                intParams[20] = int.Parse(row[31]);
+                intParams[21] = int.Parse(row[32]);
+                contParams = new double[15];
+                for (int i = 0; i < 7; i++)
+                {
+                    contParams[i] = double.Parse(row[24 + i]);
+                }
+                for (int i = 0; i < 8; i++)
+                {
+                    contParams[i + 7] = double.Parse(row[33 + i]);
+                }
+                
+            }
+            public int Duration;
+            public Protocol Output;
+            public Protocol Input;
+            public readonly int[] intParams; //4 - 23, 31, 32
+            public readonly double[] contParams; //24 - 30, 33-40
+
+            public GenericConnectionType GenConType;
+            public MiddleSpecificConnectionType MidConType;
+            public SpecificConnectionType ConType;
+
+        }
+
         private void learnThreadProcedure()
         {
-            
+            var learnExamples = new string[LineCount][];
+
+            using (var f = new StreamReader(FilePath))
+            {
+                //skip to needed line
+                for (int i = 0; i < FromLine && Active; i++) f.ReadLine();
+
+                //read lines to string
+                for (int i = 0; i < LineCount && Active; i++) learnExamples[i] = f.ReadLine().Split(',');
+
+                var a = (from s in learnExamples select s[2]).Distinct().ToArray();
+                var b = (from s in learnExamples select s[3]).Distinct().ToArray();
+            }
+            Active = false;
         }
     }
 }
---
+++ kddNeural/RowHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kddNeural
+{
+    public enum GenericConnectionType
+    {
+        normal, suspicious
+    };
+
+    public enum MiddleSpecificConnectionType
+    {
+        normal, probe, u2r, r2l, dos
+    };
+
+    public enum SpecificConnectionType
+    {
+        normal, back, buffer_overflow,
+        ftp_write, guess_passwd, imap,
+        ipsweep, land, loadmodule,
+        multihop, neptune, nmap, perl, phf,
+        pod, portsweep, rootkit, satan, smurf,
+        spy, teardrop, warezclient, warezmaster
+    };
+    public enum Protocol
+    {
+        http, smtp, finger, domain_u, auth, telnet,
+        ftp, eco_i, ntp_u, ecr_i, other
+    };
+    public static class RowHelper
+    {
+        private static readonly Dictionary<SpecificConnectionType, MiddleSpecificConnectionType> dict =
+            new Dictionary<SpecificConnectionType, MiddleSpecificConnectionType>()
+            {
+                {SpecificConnectionType.back,            MiddleSpecificConnectionType.dos},
+                {SpecificConnectionType.buffer_overflow, MiddleSpecificConnectionType.u2r},
+                {SpecificConnectionType.ftp_write,       MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.guess_passwd,    MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.imap,            MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.ipsweep,         MiddleSpecificConnectionType.probe},
+                {SpecificConnectionType.land,            MiddleSpecificConnectionType.dos},
+                {SpecificConnectionType.loadmodule,      MiddleSpecificConnectionType.u2r},
+                {SpecificConnectionType.multihop,        MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.neptune,         MiddleSpecificConnectionType.dos},
+                {SpecificConnectionType.nmap,            MiddleSpecificConnectionType.probe},
+                {SpecificConnectionType.perl,            MiddleSpecificConnectionType.u2r},
+                {SpecificConnectionType.phf,             MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.pod,             MiddleSpecificConnectionType.dos},
+                {SpecificConnectionType.portsweep,       MiddleSpecificConnectionType.probe},
+                {SpecificConnectionType.rootkit,         MiddleSpecificConnectionType.u2r},
+                {SpecificConnectionType.satan,           MiddleSpecificConnectionType.probe},
+                {SpecificConnectionType.smurf,           MiddleSpecificConnectionType.dos},
+                {SpecificConnectionType.spy,             MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.teardrop,        MiddleSpecificConnectionType.dos},
+                {SpecificConnectionType.warezclient,     MiddleSpecificConnectionType.r2l},
+                {SpecificConnectionType.warezmaster,     MiddleSpecificConnectionType.r2l}
+            };
+        public static T GetEnumFromString<T>(string s)
+        {
+            return (T)Enum.Parse(typeof(T), s);
+        }
+
+        public static GenericConnectionType GetGenericConnectionType(string s)
+        {
+            if (s.Contains("normal")) return GenericConnectionType.normal;
+            return GenericConnectionType.suspicious;
+        }
+
+        public static MiddleSpecificConnectionType GetMiddleSpecificConnectionType(string s)
+        {
+            var specific = GetEnumFromString<SpecificConnectionType>(s);
+
+            return dict[specific];
+        }
+
+    }
+}
